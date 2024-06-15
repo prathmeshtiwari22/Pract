55c9b69297d37d7b62a468f503bb686c21871915
prathmeshtiwari22 <122382508+prathmeshtiwari22@users.noreply.github.com>
2024-06-15 17:08:57 +0530

    Create peekstack.c

---
+++ peekstack.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Stack{
+  int top;
+  int size;
+  int *arr;
+};
+void push(struct Stack* ptr,int val){
+    if(isFull(ptr)){
+        printf("STACK OVERFLOW \n");
+    }
+    else{
+        ptr->top++;
+        ptr->arr[ptr->top]=val;
+    }
+}
+int isEmpty(struct Stack* ptr){
+    if(ptr->top==-1){
+        return 1;
+    }
+    return 0;
+}
+int isFull(struct Stack* ptr){
+    if(ptr->top==ptr->size-1){
+        return 1;
+    }
+    return 0;
+}
+int peek(struct Stack* ptr,int index){
+    if(ptr->top-index+1 < 0){
+        return -1;
+    }
+    return ptr->arr[ptr->top-index+1];
+}
+int main(){
+    struct Stack* s=(struct Stack*)malloc(sizeof(struct Stack));
+    s->size=10;
+    s->top=-1;
+    s->arr=(int*)malloc(s->size*sizeof(int));
+    push(s,12);
+    push(s,23);
+    push(s,34);
+    push(s,45);
+    for(int i=1;i<=s->top+1;i++){
+        printf("PEEK OF ELEMENT %d will be %d \n",i,peek(s,i));
+    }
+    return 0;
+}
