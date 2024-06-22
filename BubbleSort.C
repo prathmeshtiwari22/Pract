dbf33922c614f2d1854c50905b8f2476851ea16e
prathmeshtiwari22 <122382508+prathmeshtiwari22@users.noreply.github.com>
2024-06-22 18:09:30 +0530

    Update BubbleSort.C

--- BubbleSort.C
+++ BubbleSort.C
@@ -1,21 +1,21 @@
 #include<stdio.h>
-void Display(int* A,int n){
+void Display(int *A,int n){
     for(int i=0;i<n;i++){
-     printf("%d  ",A[i]);   
+        printf("%d  ",A[i]);
     }
-    printf("\n");
+    printf(" \n ");
 }
-void BubbleSort(int* A,int n){
+void BubbleSort(int *A,int n){
     int temp,isSorted;
-    for(int i=0;i<n-1;i++){
-        printf("Pass %d on the Array \n",i);
+    for(int i=0; i<n-1;i++){
+        printf("AT EACH PASS %d  \n",i);
         isSorted=1;
         for(int j=0;j<n-1-i;j++){
             if(A[j]>A[j+1]){
-               temp=A[j];
-               A[j]=A[j+1];
-               A[j+1]=temp;
-               isSorted=0;
+                temp=A[j];
+                A[j]=A[j+1];
+                A[j+1]=temp;
+                isSorted=0;
             }
         }
         if(isSorted){
@@ -30,12 +30,4 @@ int main(){
     BubbleSort(A,n);
     Display(A,n);
 }
-
-// 232  223  334  21  22  11  21  31  
-//Pass 0 on the Array 
-//Pass 1 on the Array 
-//Pass 2 on the Array 
-//Pass 3 on the Array 
-//Pass 4 on the Array 
-//Pass 5 on the Array 
-// 11  21  21  22  31  223  232  334  
+//Timecomplexity O(n2) SpaceComplexity O(n2)
