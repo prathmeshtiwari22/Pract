53f74d7f3b8bda872845f27735736dd41753878d
prathmeshtiwari22 <122382508+prathmeshtiwari22@users.noreply.github.com>
2024-06-27 16:21:28 +0530

    Create Inrder.c

---
+++ Inrder.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node{
+    int Data;
+    struct Node* left;
+    struct Node* right;
+};
+struct Node* createNode(int Data){
+    struct Node* n=(struct Node*)malloc(sizeof(struct Node));
+    n->Data=Data;
+    n->left=NULL;
+    n->right=NULL;
+    return n;
+}
+void InOrder(struct Node* root){
+    if(root!=NULL){
+        InOrder(root->left);
+        printf(" %d  ",root->Data);
+        InOrder(root->right);
+    }
+}
+int main(){
+    struct Node* p= createNode(4);
+    struct Node* p1=createNode(1);
+    struct Node* p2=createNode(6);
+    struct Node* p3=createNode(5);
+    struct Node* p4=createNode(2);
+    //  4
+    // 1  6
+//  5   2    
+    p->left=p1;
+    p->right=p2;
+    p1->left=p3;
+    p1->right=p4;
+    InOrder(p);
+    return 0;
+}
